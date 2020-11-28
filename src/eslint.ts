b4f9c812c44acfe1a3572952c85a3eb70f5950f7
fupeng <fupenglxy@gmail.com>
2020-11-28 14:28:59 +0800

    chore: update eslint

--- src/eslint.ts
+++ src/eslint.ts
@@ -20,5 +20,15 @@ module.exports = {
     'no-void': 0,
     'no-empty': [2, { allowEmptyCatch: true }],
     'prefer-promise-reject-errors': [2, { allowEmptyReject: true }],
+    'import/order': 1,
+    '@typescript-eslint/no-use-before-define': [
+      2,
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
+    '@typescript-eslint/explicit-function-return-type': [
+      2,
+      { allowTypedFunctionExpressions: true },
+    ],
+    '@typescript-eslint/camelcase': 0,
   },
 };
