a5f2466ff4cd47db54c1910808a76c7708ea4603
fupeng <fupenglxy@gmail.com>
2021-09-03 01:48:24 +0800

    chore: deps

--- src/eslint.ts
+++ src/eslint.ts
@@ -1,4 +1,10 @@
 module.exports = {
+  extends: [
+    'react-app',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
   env: {
     browser: true,
     node: true,
@@ -7,12 +13,6 @@ module.exports = {
     jest: true,
     jasmine: true,
   },
-  extends: [
-    'react-app',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:prettier/recommended',
-    'prettier',
-  ],
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     'no-void': 0,
@@ -27,33 +27,6 @@ module.exports = {
       { allowTypedFunctionExpressions: true },
     ],
     '@typescript-eslint/camelcase': 0,
-    'import/order': [
-      // import 分组排序
-      'error',
-      {
-        groups: [
-          'builtin', // Built-in types are first
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          // ['parent', 'sibling'], // Then sibling and parent types. They can be mingled together
-          'index', // Then the index file
-          'object', // Then the rest: internal and external type
-        ],
-        'newlines-between': 'always',
-      },
-    ],
-    'padding-line-between-statements': [
-      // 块级前后加空行，表达式前后加空行
-      'error',
-      { blankLine: 'always', prev: 'block-like', next: '*' },
-      { blankLine: 'always', prev: '*', next: 'block-like' },
-      { blankLine: 'always', prev: 'multiline-expression', next: '*' },
-      { blankLine: 'always', prev: '*', next: 'multiline-expression' },
-      { blankLine: 'always', prev: '*', next: 'return' },
-      { blankLine: 'always', prev: 'import', next: 'expression' },
-    ],
-    'no-else-return': ['error', { allowElseIf: false }], // if return 则去掉else
+    'no-else-return': [2, { allowElseIf: false }], // if return 则去掉else
   },
 };
